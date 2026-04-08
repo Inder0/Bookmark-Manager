88ff5614997ba9c144b750ff82b7932d65f28e22
Inderpreet <inderpreet.singh0511@gmail.com>
2026-04-08 13:43:30 +0530

    add required models

---
+++ bookmarks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Bookmark(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    url=models.URLField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    favicon=models.URLField(blank=True)
+    def __str__(self):
+        return self.title or self.url
---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import logout
+from .forms import UserRegisterForm
+
+# Create your views here.
+def register(request):
+    form=UserRegisterForm(request.POST or None)
+    if request.method=='POST':
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f"Welcome {username}, your account has been successfully created")
+            return redirect('login')
+    context={
+        'form':form
+    }
+    return render(request,"users/register.html",context)
+
+def logut_view(request):
+    logout(request)
+    return render(request,"users/logout.html")
